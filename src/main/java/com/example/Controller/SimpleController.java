fa20c839705efa5aff204a8ec8bff4dc5ba7e0d0
SYED UMAR <umar630934@gmail.com>
2024-08-30 22:25:07 +0530

    fix: commit

--- src/main/java/com/example/Controller/SimpleController.java
+++ src/main/java/com/example/Controller/SimpleController.java
@@ -1,14 +1,9 @@
 package com.example.Controller;
 
-import java.net.http.HttpResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
